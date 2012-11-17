c78495c25db6623beb3a0384c664e389fc1e4340
Jonathan Thuau <jonathan.thuau@gmail.com>
2012-11-17 10:46:50 +0100

    [UPDATE] jQuery Cookie Plugin
    1.3

--- assets/js/jquery.cookie.js
+++ assets/js/jquery.cookie.js
@@ -1,6 +1,5 @@
-/*jshint eqnull:true */
 /*!
- * jQuery Cookie Plugin v1.2
+ * jQuery Cookie Plugin v1.3
  * https://github.com/carhartl/jquery-cookie
  *
  * Copyright 2011, Klaus Hartl
@@ -49,7 +48,8 @@
 		// read
 		var decode = config.raw ? raw : decoded;
 		var cookies = document.cookie.split('; ');
-		for (var i = 0, parts; (parts = cookies[i] && cookies[i].split('=')); i++) {
+		for (var i = 0, l = cookies.length; i < l; i++) {
+			var parts = cookies[i].split('=');
 			if (decode(parts.shift()) === key) {
 				var cookie = decode(parts.join('='));
 				return config.json ? JSON.parse(cookie) : cookie;
